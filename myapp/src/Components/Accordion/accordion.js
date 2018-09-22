3dbdf64a60c735bd609cfc12fe3fa2083ef39813
chandrashekar <chandrashekarballa920@gmail.com>
2018-09-22 15:32:22 +0530

    discover

---
+++ myapp/src/Components/Accordion/accordion.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Style/style.css'
+export default class Accordion extends React.Component{
+
+    render(){
+        return(
+            <React.Fragment>
+                <div className="accordioan">
+                 <h1>accordioan</h1>
+
+                </div>
+                <img src='./Components/images/indiaflag.png' alt="IndiaFlag"/>
+            </React.Fragment>
+        );
+    }
+
+
+}
